570a3f388bf92c5a4a8338b5bbe5c89ba168b702
Yudi Yamane <yudiyamane@gmail.com>
2021-01-31 02:55:16 -0300

    working better than my first attemp years ago

--- src/main.c
+++ src/main.c
@@ -9,12 +9,21 @@ void clear_screen() {
 }
 
 int main() {
-  int remaining_mines = MINES_TO_START;
+  // talvez first_i e first_j sejam melhores
+  int i, j, not_allowed_i, not_allowed_j;
+  int remaining_uncovered_spots = 
+    FIELD_SIZE*FIELD_SIZE - INITIAL_MINE_COUNT;
 
-  init_fields(minefield_hidden, minefield_visible);
+  init_field_visible(minefield_visible);
+
+  print_field(minefield_visible);
+  scanf("%d %d", &not_allowed_i, &not_allowed_j);
+  // validar 
+
+  init_field_hidden(minefield_hidden, not_allowed_i, not_allowed_j);
+  reveal_zeros_at(not_allowed_i, not_allowed_j, minefield_hidden, minefield_visible);
   
-  int i, j;
-  while(remaining_mines > 0) {
+  while(remaining_uncovered_spots) {
     clear_screen();
     // print_field(minefield_visible);
     debug_print_field(minefield_hidden, minefield_visible);
@@ -29,11 +38,12 @@ int main() {
     }
     else {
       // revelar mais área
-      remaining_mines--;
+      reveal_zeros_at(i, j, minefield_hidden, minefield_visible);
+      remaining_uncovered_spots--;
     }
   }
 
-  if(remaining_mines <= 0) {
+  if(remaining_uncovered_spots <= 0) {
     printf("\n\n\tParabéns, você venceu!!!\n\n");
   }
   else {
--- src/minefield.h
+++ src/minefield.h
@@ -7,9 +7,10 @@
 
 
 #define FIELD_SIZE 8
-#define MINES_TO_START 6
+#define INITIAL_MINE_COUNT 6
 #define MINE_SYMBOL '*'
 #define EMPTY_POSITION ' '
+#define VISITED_POSITION ' '
 #define INITIAL_SYMBOL_FIELD_VISIBLE '#'
 
 
@@ -18,6 +19,40 @@ char minefield_hidden[FIELD_SIZE][FIELD_SIZE];
 
 void print_field(char field[][FIELD_SIZE]);
 
+// reveal_area_starting_from?
+void reveal_zeros_at(int i, int j, char field_hidden[][FIELD_SIZE], char field_visible[][FIELD_SIZE]) {
+  int dy, dx, dump;
+
+  for(dy = -1; dy <= 1; dy++) {
+    for(dx = -1; dx <= 1; dx++) {
+      if(0 <= i+dy && i+dy < FIELD_SIZE && 0 <= j+dx && j+dx < FIELD_SIZE) {
+        if(field_hidden[i+dy][j+dx] != MINE_SYMBOL) { // precisa dessa verificação?
+          if(field_hidden[i+dy][j+dx] == '0' && field_visible[i+dy][j+dx] != VISITED_POSITION) {
+            field_visible[i+dy][j+dx] = VISITED_POSITION;
+            reveal_zeros_at(i+dy, j+dx, field_hidden, field_visible);
+          }
+
+          if(field_visible[i+dy][j+dx] == INITIAL_SYMBOL_FIELD_VISIBLE 
+            && ('1' <= field_hidden[i+dy][j+dx] && field_hidden[i+dy][j+dx] <= '9')
+            && field_hidden[i][j] == '0'
+          ) {
+            field_visible[i+dy][j+dx] = field_hidden[i+dy][j+dx];
+          }
+
+          if('1' <= field_hidden[i][j] && field_hidden[i][j] <= '9') {
+            field_visible[i][j] = field_hidden[i][j];
+          }
+        }
+      }
+    }
+  }
+}
+
+
+/**
+ * Quem chama essa função garante que não há mina em i, j. Isso é 
+ * aconsnelhável?
+ */
 int count_mines_for_this_position(char field[][FIELD_SIZE], int i, int j) {
   int dx, dy, mines_count = 0;
 
@@ -34,6 +69,7 @@ int count_mines_for_this_position(char field[][FIELD_SIZE], int i, int j) {
   return mines_count;
 }
 
+
 void write_mine_count_for_each_position(char field_hidden[][FIELD_SIZE]) {
   int i, j, mines_count;
   for(i = 0; i < FIELD_SIZE; i++) {
@@ -46,14 +82,18 @@ void write_mine_count_for_each_position(char field_hidden[][FIELD_SIZE]) {
   }
 }
 
+
 /**
  * se o jogo resetar sem terminar, srand(time(NULL)) vai dar ruim?
+ * 
+ * not_allowed_i e not_allowed_j é a primeira posição chutada pelo jogador
+ * e lá não é permitido colocar uma mina
  */
-void place_mines(char field_hidden[][FIELD_SIZE], int mines_to_place, int seed) {
-  int i, j ;
+void place_mines(char field_hidden[][FIELD_SIZE], int mines_to_place, int not_allowed_i, int not_allowed_j, int seed) {
+  int i, j;
   // provavelmente eu deveria colocar essa parte em uma função
   for(i = 0; i < FIELD_SIZE; i++) {
-    for( j = 0; j < FIELD_SIZE; j++) {
+    for(j = 0; j < FIELD_SIZE; j++) {
       field_hidden[i][j] = EMPTY_POSITION;
     }
   }
@@ -70,18 +110,22 @@ void place_mines(char field_hidden[][FIELD_SIZE], int mines_to_place, int seed) 
     x = rand() % FIELD_SIZE;
     y = rand() % FIELD_SIZE;
 
-    if(field_hidden[y][x] != MINE_SYMBOL) {
+    if(y != not_allowed_i && x != not_allowed_i 
+      && field_hidden[y][x] != MINE_SYMBOL
+    ) {
       field_hidden[y][x] = MINE_SYMBOL;
       mines_to_place--;
     }
   }
 }
 
-void init_field_hidden(char field[][FIELD_SIZE]) {
-  int mines_to_place = 6, seed = 0;
-  place_mines(field, mines_to_place, seed);
+
+void init_field_hidden(char field[][FIELD_SIZE], int i, int j) {
+  int seed = 0; // 0 é usado para seed aleatória
+  place_mines(field, INITIAL_MINE_COUNT, i, j, seed);
   write_mine_count_for_each_position(field);
 }
+
 
 void init_field_visible(char field[][FIELD_SIZE]) {
   int i, j;
@@ -93,11 +137,6 @@ void init_field_visible(char field[][FIELD_SIZE]) {
   }
 }
 
-
-void init_fields(char hidden[][FIELD_SIZE], char visible[][FIELD_SIZE]) {
-  init_field_hidden(hidden);
-  init_field_visible(visible);
-}
 
 void print_field(char field[][FIELD_SIZE]) {
   int i, j;
@@ -112,6 +151,7 @@ void print_field(char field[][FIELD_SIZE]) {
   }
   printf("\t ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
 }
+
 
 void debug_print_field(char hidden[][FIELD_SIZE], char visible[][FIELD_SIZE]) {
     int i, j;
